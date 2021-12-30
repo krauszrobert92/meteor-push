da2f6fb24b1a136cef8a63a39a9d17ca58eef071
Robert Krausz <krauszrobert92@gmail.com>
2021-12-30 08:23:48 +0100

    update version and use cordova-plugin-push

--- package.js
+++ package.js
@@ -1,29 +1,27 @@
 /* globals Package, Npm, Cordova */
 Package.describe({
   name: 'krauszrobert92:push',
-  version: '2.0.8',
+  version: '2.0.11',
   summary: 'Push Notifications for Cordova and Web/PWA with Firebase (FCM).',
   git: 'https://github.com/activitree/meteor-push.git'
 })
 
 Npm.depends({
-  'firebase-admin': '8.13.0',
-  firebase: '7.17.2',
-  events: '3.2.0'
+  'firebase-admin': '10.0.1',
+  firebase: '9.6.1',
+  events: '3.3.0'
 })
 
 Cordova.depends({
-  'phonegap-plugin-push': '2.3.0',
+  // 'phonegap-plugin-push': '2.3.0',
+  "@havesource/cordova-plugin-push": "3.0.0",
   'cordova-plugin-device': '2.0.3'
 })
 
 Package.onUse(api => {
-  api.versionsFrom('1.8')
-  api.use(['ecmascript'])
-
+  //api.versionsFrom(['1.8', '2.3'])
   api.use(['tracker'], ['web.browser', 'web.cordova'])
-  api.use(['accounts-base@2.0.0'], ['web.browser', 'web.cordova', 'server'], { weak: true })
-  //api.use([], ['web.browser', 'web.cordova', 'server'], { weak: true }) // I removed accounts-base for meteor update
+  api.use(['accounts-base'], ['web.browser', 'web.cordova', 'server'], { weak: true })
 
   api.use([
     'ecmascript',
@@ -32,8 +30,6 @@ Package.onUse(api => {
     'ejson',
     'random'
   ], ['client', 'server'])
-
-  api.use('mongo', 'server')
 
   // API's
   api.addFiles('lib/server/pushToDevice.js', 'server')
