fb3e6671c723ba793a01544c810c99f758ae44f5
Robert Krausz <krauszrobert92@gmail.com>
2024-08-06 11:24:41 +0200

    chore: Update dependencies and add push root URL

--- package.js
+++ package.js
@@ -7,19 +7,21 @@ Package.describe({
 })
 
 Npm.depends({
-  'firebase-admin': '10.0.1',
-  firebase: '9.6.1',
+  'firebase-admin': '12.3.0',
+  firebase: '10.12.5',
   events: '3.3.0'
 })
 
 Cordova.depends({
   // 'phonegap-plugin-push': '2.3.0',
-  "@havesource/cordova-plugin-push": "3.0.1",
-  'cordova-plugin-device': '2.0.3'
+  '@havesource/cordova-plugin-push': 'https://github.com/havesource/cordova-plugin-push.git#86b52a7769fe80e975752f2d2db5b1abeb194802', // for IOS with SDK > 8.1.1
+  // '@havesource/cordova-plugin-push': '3.0.1', // for Android with SDK <=21.+
+  'cordova-plugin-device': '2.1.0'
 })
 
 Package.onUse(api => {
   //api.versionsFrom(['1.8', '2.3'])
+
   api.use(['tracker'], ['web.browser', 'web.cordova'])
   api.use(['accounts-base'], ['web.browser', 'web.cordova', 'server'], { weak: true })
 
